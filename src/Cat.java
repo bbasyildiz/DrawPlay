a2b5bdb2c4104e8841d46e995521878d005c9720
Bora Basyildiz <bbasyildiz@mymail.mines.edu>
2020-02-13 13:52:38 -0700

    black mouth and larger cat

--- src/Cat.java
+++ src/Cat.java
@@ -37,11 +37,11 @@ public class Cat {
 		x += EYE_SEPARATION;
 		g2.fillOval(x, y, EYE_WIDTH, EYE_HEIGHT);
 		// Draw the mouth
-		g2.setColor(Color.pink);
+		g2.setColor(Color.black);
 		x = catX + MOUTH_X;
 		y = catY + MOUTH_Y;
 		g2.fillOval(x, y, MOUTH_WIDTH, MOUTH_HEIGHT);
-		g2.setColor(Color.red);
+		g2.setColor(Color.DARK_GRAY);
 		// Meow text appears below cat head, +10 places below 
 		// so it doesn't overlap the drawing
 		g2.drawString("Meow Meow Meow Meow", catX, catY+HEAD_DIMENSION+20);	
